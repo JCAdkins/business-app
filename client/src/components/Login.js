96a18eee03ba23782cffa134c90db01421851ec8
Ricky Board <79424125+rboard321@users.noreply.github.com>
2022-09-23 20:56:10 -0400

    minor folder changes and Login component connected to app

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,14 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+    return(
+    <div>
 
+<h1>Welcome to login</h1>
+    </div>
+    )
 }
+
 
 export default Login
