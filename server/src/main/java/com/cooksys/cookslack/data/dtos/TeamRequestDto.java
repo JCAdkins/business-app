44cc30c3571aaf519b853068f4ed6d13bdc57f8f
Caleb Dickson <info@calebdickson.com>
2022-09-25 15:47:04 -0400

    Finish first draft of validation service. Add validation to request dtos.

--- server/src/main/java/com/cooksys/cookslack/data/dtos/TeamRequestDto.java
+++ server/src/main/java/com/cooksys/cookslack/data/dtos/TeamRequestDto.java
@@ -2,11 +2,19 @@ package com.cooksys.cookslack.data.dtos;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.validation.constraints.NotBlank;
 
 @NoArgsConstructor
 @Data
 public class TeamRequestDto {
+    @NotBlank
+    @NonNull
     private String name;
+    @NotBlank
+    @NonNull
     private String description;
+    @NonNull
     private CredentialsRequestDto credentials;
 }
