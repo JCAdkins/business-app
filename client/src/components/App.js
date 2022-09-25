ac5e16098d0c3772e21786552cea0e6632caf9da
Ricky Board <79424125+rboard321@users.noreply.github.com>
2022-09-24 21:19:24 -0400

    login, announcement, and company components basic functionality

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,16 +2,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import CompanySelect from "./CompanySelect";
 import Login from './Login'
+import Announcements from "./Announcements";
+import CreateProjects from "./CreateProjects";
 
 const App = () => {
-
+//username will be set here so that it can be passed to other components as well as Company for admin
   const [userName, setUserName] = useState()
-console.log(userName)
+  const [company, setCompany] = useState()
+  
   return (
   <Router>
     <Routes>
       <Route path="/" element={<Login setUserName={setUserName}/>} />
-      <Route path="/company" element={<CompanySelect />} />
+      <Route path="/company" element={<CompanySelect company={company} setCompany={setCompany}/>} />
+      <Route path="/announcements" element={<Announcements company={company}/>} />
+      <Route path="/createproject" element={<CreateProjects />} />
     </Routes>
 
   </Router>
