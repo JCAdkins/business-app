12a3203dc568f2fd0acfec11a8ae56deece2fc2e
Ricky Board <79424125+rboard321@users.noreply.github.com>
2022-09-26 13:27:49 -0400

    added service folder with api.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,121 +1,105 @@
-
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route, useNavigate, Navigate  } from "react-router-dom";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+  Navigate,
+} from "react-router-dom";
 
 import { useState } from "react";
 import CompanySelect from "./CompanySelect";
 
-import Login from './Login'
+import Login from "./Login";
 import Announcements from "./Announcements";
 
-import Projects from "./Projects"
-import Users from "./Users"
+import Projects from "./Projects";
+import Users from "./Users";
 
 //Dev Componenets
-import TeamOverview from './TeamOverview'
+import TeamOverview from "./TeamOverview";
 
 //CSS import
 import "../components/component-Styles/main.css";
 
 const App = () => {
-//username will be set here so that it can be passed to other components as well as Company for admin
-const [userName, setUserName] = useState()
+  //username will be set here so that it can be passed to other components as well as Company for admin
+  const [userName, setUserName] = useState();
 
-const [userData, setUserData] = useState([{
-  id: 1,
-  credentials: {
-    userName: userName,
-    admin: true
-  },
-  first: 'Ricky',
-  last: 'Board',
-  email: 'testing@yahoo.com',
-  phone: '-123-234-5432',
-  active: true,
-  team: {
-    id: 1,
-    name: 'awesome',
-    description: 'crushing it',
-    company: {
-      id: 3,
-      name: 'Apple',
-      description: 'working on products'
-    }
-  },
-  company: {
-    id: 3,
-    name: 'Apple',
-    description: 'working on products'
-  }
-}])
+  const [userData, setUserData] = useState();
 
-const [password, setPassword] = useState()
-console.log(userData)
-let navigate = useNavigate()
+  const [password, setPassword] = useState();
+  console.log(userData);
+  let navigate = useNavigate();
 
-const handleLogin = () => {
+  const handleLogin = () => {
+    //pass in user credentials and verify
+    //if user admin is true go to company select screen
+    //if user is not admin go to anounncments
+console.log('handling login')
+    
+  setUserData([
+      {
+        id: 1,
+        credentials: {
+          userName: userName,
+          admin: false,
+        },
+        first: "Ricky",
+        last: "Board",
+        email: "testing@yahoo.com",
+        phone: "-123-234-5432",
+        active: true,
+        team: {
+          id: 1,
+          name: "awesome",
+          description: "crushing it",
+          company: {
+            id: 3,
+            name: "Apple",
+            description: "working on products",
+          },
+        },
+        company: {
+          id: 3,
+          name: "Apple",
+          description: "working on products",
+        },
+      },
+    ]);
 
-  //pass in user credentials and verify
-  //if user admin is true go to company select screen
-  //if user is not admin go to anounncments
+    // if(userData.credentials.admin === true){
+    //   navigate("/company")
+    // }
 
-  setUserData([{
-    id: 1,
-    credentials: {
-      userName: userName,
-      admin: true
-    },
-    first: 'Ricky',
-    last: 'Board',
-    email: 'testing@yahoo.com',
-    phone: '-123-234-5432',
-    active: true,
-    team: {
-      id: 1,
-      name: 'awesome',
-      description: 'crushing it',
-      company: {
-        id: 3,
-        name: 'Apple',
-        description: 'working on products'
-      }
-    },
-    company: {
-      id: 3,
-      name: 'Apple',
-      description: 'working on products'
-    }
-  }])
+    localStorage.setItem("userData", JSON.stringify(userData[0]));
 
-  // if(userData.credentials.admin === true){
-  //   navigate("/company")
-  // }
+    userData[0].credentials.admin
+      ? navigate("/company")
+      : navigate("/announcements");
+  };
 
-  localStorage.setItem("userData", JSON.stringify(userData[0]))
-
-  userData[0].credentials.admin ? navigate("/company") : navigate("/announcements")
-};
-  
   return (
-  
     <Routes>
-
-      <Route path="/" element={<Login handleLogin={handleLogin} setUserName={setUserName} setPassword={setPassword}/>} />
-      <Route path="/company" element={<CompanySelect userData={userData}/>} />
-
-      
+      <Route
+        path="/"
+        element={
+          <Login
+            handleLogin={handleLogin}
+            setUserName={setUserName}
+            setPassword={setPassword}
+          />
+        }
+      />
+      <Route path="/company" element={<CompanySelect userData={userData} />} />
       <Route path="/TeamOverview" element={<TeamOverview />} />
-
-      
-      <Route path="/announcements" element={<Announcements userData={userData} />} />
-     
+      <Route
+        path="/announcements"
+        element={<Announcements userData={userData} />}
+      />
       <Route path="/projects" element={<Projects />} />
-        <Route path="/users" element={<Users />} />
-
+      <Route path="/users" element={<Users />} />
     </Routes>
-
- 
-
   );
 };
 
---
+++ client/src/services/api.js
@@ -0,0 +1,39 @@
+import toPairs from 'lodash/toPairs'
+const COMPANY_ROOT = 'https://'
+
+const parseJSON = response => {
+    if (response.status === 204 || response.status === 205) {
+      return null
+    }
+    return response.json()
+  }
+
+const checkStatus = response => {
+    if (response.status >= 200 && response.status < 300) {
+      return response
+    }
+    const error = new Error(response.statusText)
+    error.response = response
+    throw error
+  }
+
+export const request = (url, options) => {
+    return fetch(url, options)
+      .then(checkStatus)
+      .then(parseJSON)
+  }
+
+const fetchFromCompany = ({ token, endpoint, params }) => {
+    let url = [COMPANY_ROOT, endpoint].join('/')
+    if (params) {
+      const paramString = toPairs(params)
+        .map(param => param.join('='))
+        .join('&')
+      url += `?${paramString}`
+    }
+    const options = { headers: { Authorization: `Bearer ${token}` } }
+    console.log("API>>>",url)
+    return request(url, options)
+  }
+  
+  export default fetchFromCompany
