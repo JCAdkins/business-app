8441f54ed1ac38bfcba4d0a677b99b4b5870388a
StevenVallarsa <orionsjv@gmail.com>
2022-09-27 22:49:38 -0400

    working changes to Projects and Project

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -2,27 +2,24 @@ import React from "react";
 
 import { Card, CardContent, Typography, Button } from "@mui/material";
 
-const Project = ({ project, isAdmin }) => {
+const Project = ({ project, handleClick }) => {
   const millisecondsInDay = 86400000;
   const elapsedDays = Math.floor((Date.now() - project["last-edited"]) / millisecondsInDay);
 
-  const container = {
-    textAlign: "left",
-  };
   return (
     <Card
       style={{ color: "white", backgroundColor: "transparent", fontFamily: "helvetica" }}
       sx={{ boxShadow: 0, borderRadius: 0 }}
     >
-      <div style={{ display: "flex", flexDirection: "row", alignItems: "flex-end", ...container }}>
-        <div style={{ width: "75%", padding: 10 }}>
-          <p style={{ fontSize: 24, fontWeight: "bold" }}>
-            {project.title}{" "}
-            <span style={{ fontSize: 16 }}>
+      <div style={{ display: "flex", flexDirection: "row", alignItems: "flex-end", textAlign: "left" }}>
+        <div style={{ width: "80%", margin: "20px 0" }}>
+          <p style={{ fontSize: 24, fontWeight: "bold", lineHeight: 0 }}>
+            {project.name}{" "}
+            <span style={{ fontSize: 13 }}>
               Last edited {elapsedDays} day{elapsedDays === 1 ? "" : "s"} ago
             </span>
           </p>
-          <p style={{ fontSize: 16 }}>{project.description}</p>
+          <p style={{ fontSize: 13 }}>{project.description}</p>
         </div>
         <div
           style={{
@@ -30,11 +27,19 @@ const Project = ({ project, isAdmin }) => {
             display: "flex",
             flexDirection: "row",
             justifyContent: "flex-end",
-            padding: "0 0 20px 0",
+            paddingBottom: 20,
           }}
         >
           <Button
-            style={{ textTransform: "none", fontSize: 12, width: "80px", height: "25px", backgroundColor: "salmon" }}
+            style={{
+              textTransform: "none",
+              fontSize: 13,
+              width: "80px",
+              height: "25px",
+              borderRadius: 8,
+              backgroundColor: "antiquewhite",
+            }}
+            onClick={() => handleClick(project.id)}
           >
             Edit
           </Button>
